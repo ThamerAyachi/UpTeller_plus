b5274be779e53290f305c5aa0d230b4bc8e61621
Thamer Ayachi <thamerjak3@gmail.com>
2022-11-04 18:22:21 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <TailwindProvider>
+        <View className="flex-1 justify-center items-center">
+          <Text>Hello in my application</Text>
+        </View>
+      </TailwindProvider>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
